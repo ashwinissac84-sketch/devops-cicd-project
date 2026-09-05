35dfb8cdb317c12233ece35413137f663a7ca012
ashwinissac84-sketch <ashwinissac84@gmail.com>
2026-09-05 12:02:13 +0530

    Initial commit: Flask app with Dockerfile

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "DevOps CI/CD Project is Live!",
+        "status": "running"
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({
+        "status": "healthy",
+        "timestamp": datetime.datetime.now().isoformat()
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
